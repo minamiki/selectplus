c26ccb3c723a5fc22f88b97fb1c38869116064a6
Yit Chun <me@minamiki.com>
2013-07-23 13:59:39 +0800

    Cleaned up code
    standardised calling of module functions with modules instead of this
    added helper function to build divs
    removed eval expressions
    ultilised Lo-dash framework
    cleaned up variable declarations

--- select-plus.js
+++ select-plus.js
@@ -17,11 +17,10 @@
 		// functions and variables used for selectplus
 		var modules = {
 			// for storing the select element data
-			selectdata: null,
-			remotedata: null,
-			elementID: null,
-			elementName: null,
-			searchVal: null,
+			data: {},
+
+			// data for the raw input element
+			element: {},
 
 			// for keeping track the remote feed
 			remote: {
@@ -29,29 +28,20 @@
 				eof: false
 			},
 
-			// pointers to the contructed select element
-			ui: {
-				raw: null,
-				anchor: null,
-				root: null,
-				result: null,
-				search: null,
-				selectedDisplay: null,
-				arrow: null,
-				dropdown: null
-			},
+			// pointer to the contructed select elements
+			ui: {},
 
 			// read the local select's options
 			parseElement: function(element){
-				var selectArray = new Array();
+				var selectArray = [];
 
 				// iterate through the select's children
 				element.children().each(function(){
-					
+
 					if($(this).is('option')){ // if it is an option element
 						selectArray.push({lbl: $(this).text(), val: $(this).val()});
 					}else if($(this).is('optgroup')){ // if it is an option group
-						var optgroup = new Array();
+						var optgroup = [];
 						// iterate through the option group's children
 						$(this).children().each(function(){
 							if($(this).is('option')){
@@ -61,68 +51,66 @@
 						selectArray.push({val: optgroup, lbl: $(this).attr('label')});
 					}
 				});
-				this.selectdata = selectArray;
+				modules.data.local = selectArray;
 			},
 
 			// recontruct the select element with divs
 			constructElement: function(element){
+				var id = modules.element.id;
+				var name = modules.element.name;
+
 				// anchor element
-				element.replaceWith('<div id="'+this.elementID+'-anchor" class="select-anchor"></div>');
-				this.ui.anchor = $('#'+modules.elementID+'-anchor');
+				modules.helper.divBuilder('anchor', element, 'select-anchor', id+'-anchor', true);
 
 				// root element
-				this.ui.anchor.append('<div id="'+this.elementID+'-ui" class="select-container"></div>');
-				this.ui.root = $('#'+modules.elementID+'-ui');
+				modules.helper.divBuilder('root', modules.ui.anchor, 'select-container', id+'-ui');
 
 				// original form element (now as a hidden element)
-				this.ui.root.append('<input type="hidden" id="'+modules.elementID+'" />');
-				this.ui.raw = $('#'+modules.elementID);
-				if(typeof modules.elementName !== 'undefined'){
-					this.ui.raw.attr('name', modules.elementName)
+				modules.ui.root.append('<input type="hidden" id="'+id+'" />');
+				modules.ui.raw = $('#'+id);
+				if(_.isUndefined(modules.elementName)){
+					modules.ui.raw.attr('name', modules.elementName);
 				}
 
 				// select element (defaults to first item on list)
-				this.ui.root.append('<div class="ui-select-display"></div>');
-				this.ui.selectedDisplay = $('#'+this.elementID+'-ui .ui-select-display');
-				this.ui.selectedDisplay.append(this.getFirstElement());
+				modules.helper.divBuilder('selectedDisplay', modules.ui.root, 'ui-select-display');
+				console.log(modules.ui.selectedDisplay);
+				modules.ui.selectedDisplay.append(modules.getFirstElement());
 
 				// select arrow
-				this.ui.root.append('<div class="ui-select-arrow">&#9662;</div>');
-				this.ui.arrow = $('#'+this.elementID+'-ui .ui-select-arrow');
+				modules.helper.divBuilder('arrow', modules.ui.root, 'ui-select-arrow', false, false, '&#9662;');
 
 				// wrap everything in container
-				this.ui.root.append('<div class="ui-select-dropdown"></div>');
-				this.ui.dropdown = $('#'+this.elementID+'-ui .ui-select-dropdown');
+				modules.helper.divBuilder('dropdown', modules.ui.root, 'ui-select-dropdown');
 
 				// search box
-				this.ui.dropdown.append('<input type="textbox" class="ui-searchbox" value="" />');
-				this.ui.search = $('#'+this.elementID+'-ui .ui-searchbox');
+				modules.ui.dropdown.append('<input type="textbox" class="ui-searchbox" value="" />');
+				modules.ui.search = modules.ui.root.find('.ui-searchbox');
 
 				// add a result area
-				this.ui.dropdown.append('<div class="ui-select-results"></div>');
-				this.ui.result = $('#'+this.elementID+'-ui .ui-select-results');
-				
+				modules.helper.divBuilder('result', modules.ui.dropdown, 'ui-select-results');
+
 				// populate result dropdown
-				if(!this.checkRemote()){ 
+				if(!modules.checkRemote()){
 					// for local source, show all the options
-					this.renderResult(this.selectdata);
+					modules.renderResult(modules.data.local);
 				}else{
 					// for remote source, show a hint
-					this.ui.result.append('Please enter minimum '+settings.remote.minchar+' characters');
+					modules.ui.result.append('Please enter minimum '+settings.remote.minchar+' characters');
 				}
 			},
 
 			// initialize listeners
 			initListeners: function(){
 				// searchbox
-				this.ui.search.keyup(function(){
-					modules.searchVal = $(this).val();
+				modules.ui.search.keyup(function(){
+					modules.data.searchVal = $(this).val();
 					if(modules.checkRemote()){
-						if(modules.searchVal.length >= 3){
-							modules.search(modules.searchVal);
+						if(modules.data.searchVal.length >= 3){
+							modules.search(modules.data.searchVal);
 						}
 					}else{
-						modules.search(modules.searchVal);
+						modules.search(modules.data.searchVal);
 					}
 				});
 
@@ -131,67 +119,64 @@
 					modules.ui.dropdown.hide();
 					if(!modules.checkRemote()){
 						modules.ui.search.val('');
-						modules.ui.search.trigger('keyup');						
+						modules.ui.search.trigger('keyup');
 					}
 				});
 				// stop the propagation inside
-				this.ui.root.click(function(event){
+				modules.ui.root.click(function(event){
 					event.stopPropagation();
 				});
 
 				// select element and arrow
-				this.ui.selectedDisplay.click(function(event){
+				modules.ui.selectedDisplay.click(function(event){
 					modules.focusElement();
 				});
-				this.ui.arrow.click(function(event){
+				modules.ui.arrow.click(function(event){
 					modules.focusElement();
 				});
 
 				// select option
-				$('#'+this.elementID+'-ui .ui-select-results').on('click', '.selectable', function(){
+				$('#'+modules.element.id+'-ui').find('.ui-select-results').on('click', '.selectable', function(){
 					// populate the original form element
-					$('#'+modules.elementID).val($(this).attr('data-value'));
+					$('#'+modules.element.id).val($(this).attr('data-value'));
 					modules.ui.selectedDisplay.html($(this).html());
 					modules.ui.dropdown.toggle();
 
 				});
 
 				// detect end of result scrolling for remote data (for infinite loading)
-				if(this.checkRemote()){
-					this.ui.result.bind('scroll', function(){
+				if(modules.checkRemote()){
+					modules.ui.result.bind('scroll', function(){
 						if($(this).scrollTop() + $(this).innerHeight() >= $(this)[0].scrollHeight){
 							modules.loadRemote(modules.searchVal);
 						}
-					});					
+					});
 				}
 			},
 
 			// search function
 			search: function(searchvalue){
-				if(this.checkRemote()){ // search remote data
+				if(modules.checkRemote()){ // search remote data
 					$.getJSON(settings.remote.url+'&'+settings.remote.search+'='+encodeURIComponent(searchvalue)+'&'+settings.remote.pageSize.label+'='+settings.remote.pageSize.value+'&'+settings.remote.page.label+'='+settings.remote.page.value+'&callback=?'
 					).done(function(json){
 						modules.remote.eof = false;
 						modules.remote.page = 1;
 						modules.ui.result.empty();
 						modules.renderResult(json, settings.remote.value, settings.remote.label);
-					}).fail(function(jqxhr, textStatus, error){
-						var err = textStatus+', '+error;
-						console.log("Request Failed: "+err);
-					});
+					}).fail(modules.helper.displayJSONerror);
 				}else{ // search local data
-					var result = new Array();
+					var result = [];
 
 					// iterate through the select options
-					$.each(this.selectdata, function(){
+					$.each(modules.data.local, function(){
 						if(this.val instanceof Array){ // this is an optgroup
-							var opt_result = new Array();
+							var opt_result = [];
 							$.each(this.val, function(){
 								if(this.lbl.toLowerCase().indexOf(searchvalue.toLowerCase()) >= 0){
 									opt_result.push(this);
 								}
 							});
-							if(opt_result.length != 0){
+							if(opt_result.length !== 0){
 								result.push({val: opt_result, lbl: this.lbl});
 							}
 						}else{ // this is an option
@@ -206,83 +191,86 @@
 
 			// render the result
 			renderResult: function(result, valueObj, labelObj){
-				// function defaults
-				valueObj = typeof valueObj !== 'undefined' ? valueObj : 'val';
-				labelObj = typeof labelObj !== 'undefined' ? labelObj : 'lbl';
+				var value, label;
 
-				if(this.checkRemote()){ // remove the loader slice if remote
-					this.ui.result.children().filter('span.loader').remove();
-				}else{ // reset the list if local
-					this.ui.result.empty();
-				}
-				
-				var displayResult = $('<ul></ul>');
-				var resultArray;
-
-				// determine the result array source
-				if(this.checkRemote()){
-					resultArray = eval('result.'+settings.remote.root);
+				// check if rendering for remote or local feed
+				if(modules.checkRemote()){
+					value = valueObj;
+					label = labelObj;
+					// remove the loader slice if remote
+					modules.ui.result.children().filter('span.loader').remove();
+					// array source
+					resultArray = result[settings.remote.root];
 				}else{
+					value = 'val';
+					label = 'lbl';
+					// reset the list if local
+					modules.ui.result.empty();
+					// array source
 					resultArray = result;
 				}
-				
+
+				var displayResult = $('<ul></ul>');
+				var optionResult, resultArray;
+
 				// iterate through the result
 				$.each(resultArray, function(){
-					if(eval('this.'+valueObj) instanceof Array){ // this is an optgroup
-						var optionResult = $('<li></li>');
+					if(this[value] instanceof Array){ // this is an optgroup
+						optionResult = $('<li></li>');
 						// render the optgroup label
-						optionResult.append('<span>'+eval('this.'+labelObj)+'</span>');
+						optionResult.append('<span>'+this[label]+'</span>');
 						var optionGroupResult = $('<ul></ul>');
-											
-						$.each(eval('this.'+valueObj), function(){
-							optionGroupResult.append('<li data-value="'+eval('this.'+valueObj)+'" class="selectable">'+eval('this.'+labelObj)+'</li>');
+
+						$.each(this[value], function(){
+							optionGroupResult.append('<li data-value="'+this[value]+'" class="selectable">'+this[label]+'</li>');
 						});
-						optionResult.append(optionGroupResult);					
+						optionResult.append(optionGroupResult);
 					}else{ // this is an option
-						var optionResult = '<li data-value="'+eval('this.'+valueObj)+'" class="selectable">'+eval('this.'+labelObj)+'</li>';
+						optionResult = '<li data-value="'+this[value]+'" class="selectable">'+this[label]+'</li>';
 					}
 					displayResult.append(optionResult);
 				});
 
 				// check if there are results to return
-				if(displayResult.children().length == 0){
-					if(!this.remote.eof){
-						this.ui.result.append('<ul><li>No results</li></ul>');
+				if(displayResult.children().length === 0){
+					if(!modules.remote.eof){
+						modules.ui.result.append('<ul><li>No results</li></ul>');
 					}
 				}else{
-					this.ui.result.append(displayResult);
+					modules.ui.result.append(displayResult);
 				}
 
 				// if loading from remote source, include a loading bar
-				if(this.checkRemote() && !this.remote.eof && displayResult.children().length != 0){
-					this.ui.result.append('<span class="loader">Loading...</span>');
+				if(modules.checkRemote() && !modules.remote.eof && displayResult.children().length !== 0){
+					modules.ui.result.append('<span class="loader">Loading...</span>');
 				}
-				
+
 			},
 
 			// get the first value
 			getFirstElement: function(){
-				if(this.checkRemote()){ // for remote source
+				if(modules.checkRemote()){ // for remote source
 					return settings.remote.hint;
 				}else{ // for local source
-					if(this.selectdata[0].val instanceof Array){ // optgroup
-						return this.selectdata[0].val[0].lbl
+					if(modules.data.local[0].val instanceof Array){ // optgroup
+						return modules.data.local[0].val[0].lbl;
 					}else{ // option
-						return this.selectdata[0].lbl;
-					}					
+						return modules.data.local[0].lbl;
+					}
 				}
 			},
 
 			// focus select element, this is to bring the z-index of the div to the top
 			focusElement: function(){
 				$('.select-anchor').removeClass('focus');
-				this.ui.anchor.toggleClass('focus');
+				modules.ui.anchor.toggleClass('focus');
 				modules.ui.dropdown.toggle();
+				modules.ui.search.focus();
 			},
 
 			// check if using remote data
 			checkRemote: function(){
-				if(settings.remote.url != null){
+				if(settings.remote.url !== null){
 					return true;
 				}else{
 					return false;
@@ -291,43 +279,88 @@
 
 			// load from remote
 			loadRemote: function(searchvalue){
-				if(this.checkRemote() && !modules.remote.eof){
+				if(modules.checkRemote() && !modules.remote.eof){
 					// does a JSONP request
 					$.getJSON(settings.remote.url+'&'+settings.remote.search+'='+encodeURIComponent(searchvalue)+'&'+settings.remote.pageSize.label+'='+settings.remote.pageSize.value+'&'+settings.remote.page.label+'='+modules.remote.page+'&callback=?'
 					).done(function(json){
-						if(eval('json.'+settings.remote.root+'.length') == 0){ // if no results are returned, we have reached the End of Feed
+						if(json[settings.remote.root].length === 0){ // if no results are returned, we have reached the End of Feed
 							modules.remote.eof = true;
 							modules.renderResult(json);
 						}else{
 							modules.renderResult(json, settings.remote.value, settings.remote.label);
 						}
-					}).fail(function(jqxhr, textStatus, error){
-						var err = textStatus+', '+error;
-						console.log("Request Failed: "+err);
-					});
+					}).fail(modules.helper.displayJSONerror);
 				}
 				// increment the page number so that the next load will request the next page
-				this.remote.page++;
+				modules.remote.page++;
 			},
 
 			// initialise the module
 			init: function(element){
-				if(this.checkRemote()){
-					this.remote.page = settings.remote.page.value;
+				if(modules.checkRemote()){
+					modules.remote.page = settings.remote.page.value;
 				}else{
 					modules.parseElement(element);
 				}
 
-				modules.elementID = element.attr('id');
+				modules.element.id = element.attr('id');
 				modules.elementName = element.attr('name');
 				modules.constructElement(element);
-				modules.initListeners();	
+				modules.initListeners();
+			},
+
+			// helper functions
+			helper: {
+				// display JSON errors
+				displayJSONerror: function(jqxhr, textStatus, error){
+					console.log("Request Failed: "+textStatus+', '+error);
+				},
+
+				// div builder
+				divBuilder: function(saveAs, element, classname, id, append, content){
+					var string = {
+						id: '',
+						classname: '',
+						content: '',
+						element: ''
+					};
+
+					if(!_.isUndefined(classname)){
+						string.classname = ' class="'+classname+'"';
+					}
+					if(!_.isUndefined(id)){
+						if(id !== false){
+							string.id = ' id="'+id+'"';
+						}
+					}
+					if(!_.isUndefined(content)){
+						string.content = content;
+					}
+
+					string.element = '<div'+string.id+string.classname+'>'+string.content+'</div>';
+
+					if(_.isUndefined(append)){
+						element.append(string.element);
+					}else{
+						if(!append){
+							element.append(string.element);
+						}else{
+							element.replaceWith(string.element);
+						}
+					}
+
+					if(_.isString(id)){
+						modules.ui[saveAs] = $('#'+id);
+					}else{
+						modules.ui[saveAs] = modules.ui.root.find('.'+classname);
+					}
+				}
 			}
-		}
+		};
 
 		// initialise the plugin
 		modules.init(this);
 
 		return this;
-	}
+	};
 }(jQuery));
